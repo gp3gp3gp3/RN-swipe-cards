2b5eeca8eae3f5a01fe7c4a5106b335c78d51d15
George Paul <george.allen.paul.3@gmail.com>
2017-12-10 09:24:47 -0500

    Animated ball

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import Ball from './src/Ball'
 
 export default class App extends React.Component {
@@ -8,15 +8,13 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Ball />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: '#fff'
+  }
+})
