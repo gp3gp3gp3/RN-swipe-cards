292ed8d6eeb96ca62b2314ab9cc7b35c745e5c9b
George Paul <george.allen.paul.3@gmail.com>
2017-12-11 08:08:17 -0500

    Add rotating card

--- src/Deck.js
+++ src/Deck.js
@@ -23,12 +23,27 @@ class Deck extends Component {
 
     this.state = { panResponder, position }
   }
+
+  getCardStyle () {
+    const { position } = this.state
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0, 500],
+      outputRange: ['-120deg', '0deg', '120deg']
+    })
+    
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    }
+  }
+  
   renderCards () {
     return this.props.data.map((item, index) => {
       if (index === 0) {
         return (
           <Animated.View
-            style={this.state.position.getLayout()}
+            key={item.id}
+            style={this.getCardStyle()}
             {...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
